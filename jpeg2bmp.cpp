2f9b9eff76872c2f0ff57c4888511ed9fe632be8
aliakseis <aliaksei.sanko@gmail.com>
2018-07-05 12:03:09 +0300

    Initial commit

---
+++ jpeg2bmp.cpp
@@ -0,0 +1,273 @@
+// jpeg2bmp.cpp : Defines the entry point for the console application.
+//
+
+#include "kdtree.h"
+
+#include <boost/gil/extension/io/jpeg_io.hpp>
+
+#include <iostream>
+
+#include <array>
+#include <set>
+#include <vector>
+#include <random>
+#include <limits.h>
+#include <float.h>
+#include <math.h>
+
+#include <Windows.h>
+
+const WORD DIB_HEADER_MARKER = ('M' << 8) | 'B';
+
+template<typename T1, typename T2>
+bool CreateBitmap(LPCSTR lpFileName, int g_nWidth, int g_nHeight, T1 paletteFunc, T2 pixelsFunc)
+{
+    HANDLE hFile = ::CreateFileA(
+        lpFileName,						// pointer to name of the file
+        GENERIC_READ | GENERIC_WRITE,   // access (read-write) mode
+        0,								// share mode 
+        NULL,							// pointer to security attributes 
+        CREATE_ALWAYS,					// how to create 
+        FILE_ATTRIBUTE_NORMAL,		    // file attributes 
+        NULL							// handle to file with attributes to copy
+    );
+    if (INVALID_HANDLE_VALUE == hFile)
+        return false;
+
+    g_nWidth = (g_nWidth + 3) & ~3; // Should be normalized
+
+    enum {
+        DataOffset = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER)
+        + sizeof(RGBQUAD) * 256
+    };
+    int nTotalSize = DataOffset + g_nWidth * g_nHeight;
+
+    HANDLE hFileMapping = ::CreateFileMapping(
+        hFile,			// handle to file to map 
+        NULL,			// optional security attributes 
+        PAGE_READWRITE, // protection for mapping object 
+        0,				// high-order 32 bits of object size 
+        nTotalSize,		// low-order 32 bits of object size 
+        NULL			// name of file-mapping object 
+    );
+    if (NULL == hFileMapping)
+    {
+        ::CloseHandle(hFile);
+        return false;
+    }
+    LPVOID pBuf = ::MapViewOfFile(hFileMapping, FILE_MAP_WRITE, 0, 0, nTotalSize);
+    if (NULL == pBuf)
+    {
+        ::CloseHandle(hFile);
+        ::CloseHandle(hFileMapping);
+        return false;
+    }
+
+    BITMAPFILEHEADER* pBitmapFileHeader = (BITMAPFILEHEADER*)pBuf;
+    pBitmapFileHeader->bfType = DIB_HEADER_MARKER;
+    pBitmapFileHeader->bfSize = nTotalSize;
+    pBitmapFileHeader->bfReserved1 = 0;
+    pBitmapFileHeader->bfReserved2 = 0;
+    pBitmapFileHeader->bfOffBits = DataOffset;
+
+    BITMAPINFOHEADER* pBitmapInfoHeader
+        = (BITMAPINFOHEADER*)(((BITMAPFILEHEADER*)pBuf) + 1);
+    memset(pBitmapInfoHeader, 0, sizeof(BITMAPINFOHEADER));
+
+    pBitmapInfoHeader->biSize = sizeof(BITMAPINFOHEADER);
+    pBitmapInfoHeader->biWidth = g_nWidth;
+    pBitmapInfoHeader->biHeight = g_nHeight;
+    pBitmapInfoHeader->biPlanes = 1;
+    pBitmapInfoHeader->biBitCount = 8;
+    pBitmapInfoHeader->biCompression = BI_RGB;
+
+    RGBQUAD* pRGBQUAD = (RGBQUAD*)(pBitmapInfoHeader + 1);
+
+    paletteFunc(pRGBQUAD);
+
+    BYTE* g_pBytes = ((BYTE*)pBuf) + DataOffset;
+
+    pixelsFunc(g_pBytes, g_nWidth);
+
+    ::UnmapViewOfFile(pBuf);
+    ::CloseHandle(hFile);
+    ::CloseHandle(hFileMapping);
+
+    return true;
+}
+
+// C:\boost_1_63_0\libs\gil\example\test.jpg
+
+namespace gil = boost::gil;
+
+typedef std::array<unsigned char, 3> pixel;
+
+std::set<pixel> getInitialPoints(const gil::rgb8_image_t& img, int seed)
+{
+    const auto& view = const_view(img);
+
+    const auto size = view.size();
+
+    std::vector<int> indices(size);
+
+    std::default_random_engine dre(seed);
+    for (int i = 0; i < size; ++i)
+    {
+        std::uniform_int_distribution<int> di(0, i);
+        const int j = di(dre);
+        if (i != j)
+            indices[i] = indices[j];
+        indices[j] = i;
+    }
+
+    std::set<pixel> result;
+    for (const auto idx : indices)
+    {
+        gil::rgb8_pixel_t px = *view.at(idx);
+        if (result.insert({ px[0], px[1], px[2] }).second
+            && result.size() == 256)
+        {
+            break;
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "Usage: jpeg2bmp input_file output_file\n";
+        return 1;
+    }
+
+    gil::rgb8_image_t img;
+    gil::jpeg_read_image(argv[1], img);
+
+    enum { NUM_ATTEMPTS = 10 };
+
+    ObjectInfos storedInfos[NUM_ATTEMPTS];
+    kdnode *storedRoots[NUM_ATTEMPTS];
+    double storedErrors[NUM_ATTEMPTS];
+
+    const auto& view = const_view(img);
+
+#pragma omp parallel for
+    for (int attempt = 0; attempt < NUM_ATTEMPTS; ++attempt)
+    {
+        ObjectInfos infos;
+
+        for (const auto& point : getInitialPoints(img, attempt))
+        {
+            infos.push_back({ point });
+        }
+
+        double error = DBL_MAX;
+
+        for (;;)
+        {
+            std::vector<ObjectInfo*> infoPtrs;
+            infoPtrs.reserve(infos.size());
+
+            for (auto it = infos.begin(); it != infos.end(); ++it)
+            {
+                infoPtrs.push_back(&*it);
+            }
+
+            auto root = insert(infoPtrs.begin(), infoPtrs.end(), nullptr, 0);
+
+            for (gil::rgb8_pixel_t px : view)
+            {
+                SearchResults result;
+                bool flags[DIM * 2]{};
+                pixel data{ px[0], px[1], px[2] };
+                kd_nearest_i_nearer_subtree(root, data.data(), result, flags, 0);
+                auto& a = result.node()->data;
+                std::transform(a.begin(), a.end(), data.begin(), a.begin(), std::plus<DistanceType>());
+                auto& sq_a = result.node()->sq_data;
+                std::transform(sq_a.begin(), sq_a.end(), data.begin(), sq_a.begin(), 
+                    [](auto left, auto right) { return left + right * right; });
+                ++result.node()->count;
+            }
+
+            double newError = 0;
+            for (const auto& item : infos)
+            {
+                const double count = item.count;
+                const auto delta 
+                        = item.sq_data[0] - item.data[0] * (item.data[0] / count)
+                        + item.sq_data[1] - item.data[1] * (item.data[1] / count)
+                        + item.sq_data[2] - item.data[2] * (item.data[2] / count);
+                newError += delta;
+            }
+
+
+            if (newError >= error)
+                break;
+
+            error = newError;
+
+            ObjectInfos newInfos;
+
+            for (const auto& item : infos)
+            {
+                pixel px{
+                    (item.data[0] + item.count / 2) / item.count,
+                    (item.data[1] + item.count / 2) / item.count,
+                    (item.data[2] + item.count / 2) / item.count };
+                newInfos.push_back({ px });
+            }
+
+            storedRoots[attempt] = root;
+            storedErrors[attempt] = error;
+            storedInfos[attempt] = std::move(infos);
+            infos = std::move(newInfos);
+        }
+
+        //std::cout << attempt << ' ' << error << '\n';
+    }
+
+    int optimalIdx = 0;
+    double minError = storedErrors[0];
+    for (int i = 1; i < NUM_ATTEMPTS; ++i)
+    {
+        if (storedErrors[i] < minError)
+            optimalIdx = i;
+    }
+
+    auto root = storedRoots[optimalIdx];
+    const ObjectInfos& infos = storedInfos[optimalIdx];
+
+    const auto width = img.width();
+    const auto height = img.height();
+
+    CreateBitmap(argv[2], width, height,
+        [&infos](RGBQUAD* pRGBQUAD) {
+            for (int i = 0; i < 256; i++, pRGBQUAD++)
+            {
+                pRGBQUAD->rgbRed = infos[i].pos[0];
+                pRGBQUAD->rgbGreen = infos[i].pos[1];
+                pRGBQUAD->rgbBlue = infos[i].pos[2];
+                pRGBQUAD->rgbReserved = 0;
+            }
+        },
+        [&view, root, &infos](BYTE* pBytes, int pitch) {
+            const auto width = view.width();
+            const auto height = view.height();
+            for (int i = 0; i < height; ++i)
+                for (int j = 0; j < width; ++j)
+                {
+                    gil::rgb8_pixel_t px = *view.at(j, height - 1 - i);
+
+                    SearchResults result;
+                    bool flags[DIM * 2]{};
+                    pixel data{ px[0], px[1], px[2] };
+                    kd_nearest_i_nearer_subtree(root, data.data(), result, flags, 0);
+                    auto v = result.node() - infos.data();
+                    pBytes[i * pitch + j] = (BYTE)v;
+                }
+        });
+
+    return 0;
+}
---
+++ kdtree.h
@@ -0,0 +1,235 @@
+﻿#pragma once
+
+#include <algorithm>
+#include <array>
+#include <vector>
+
+
+enum { DIM = 3 };
+
+typedef unsigned char AttributeType;
+
+typedef int DistanceType;
+
+inline DistanceType SQ(DistanceType x) { return x * x; }
+
+struct kdnode
+{
+    std::array<AttributeType, DIM> pos;
+    std::array<DistanceType, DIM> data;
+    std::array<unsigned long long, DIM> sq_data;
+    int count;
+    kdnode *left, *right;
+};
+
+typedef kdnode ObjectInfo;
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+typedef std::vector<ObjectInfo> ObjectInfos;
+
+struct IsInfoLess
+{
+    IsInfoLess(int idx) : m_idx(idx) {}
+
+    bool operator() (const ObjectInfo& x, const ObjectInfo& y) const
+    {
+        return x.pos[m_idx] < y.pos[m_idx];
+    }
+    bool operator() (const ObjectInfo* x, const ObjectInfo* y) const
+    {
+        return x->pos[m_idx] < y->pos[m_idx];
+    }
+private:
+    int m_idx;
+};
+
+kdnode* insert(std::vector<ObjectInfo*>::iterator begin,
+    std::vector<ObjectInfo*>::iterator end,
+    kdnode* parent,
+    int dir)
+{
+    if (begin == end)
+        return 0;
+
+    int diff = int(end - begin);
+    if (1 == diff)
+    {
+        kdnode* node = *begin;
+
+        node->left = 0;
+        node->right = 0;
+
+        return node;
+    }
+
+    int halfSize = diff / 2;
+
+    auto middle = begin + halfSize;
+
+    std::nth_element(begin, middle, end, IsInfoLess(dir));
+
+    kdnode* node = *middle;
+
+    const auto new_dir = (dir + 1) % DIM;
+    node->left = insert(begin, middle, node, new_dir);
+    node->right = insert(++middle, end, node, new_dir);
+
+    return node;
+}
+
+
+struct SearchResult
+{
+    DistanceType dist_sq;
+    SearchResult* next;
+    int data;
+};
+
+bool operator < (const SearchResult& left, const SearchResult& right)
+{
+    return left.dist_sq < right.dist_sq;
+}
+
+bool operator < (const SearchResult& left, DistanceType right)
+{
+    return left.dist_sq < right;
+}
+
+bool operator < (DistanceType left, const SearchResult& right)
+{
+    return left < right.dist_sq;
+}
+
+
+class SearchResults
+{
+public:
+    SearchResults() : m_full(false), m_dist_sq(0.), m_node(nullptr) {}
+    bool isFull() const { return m_full; }
+    double dist_sq() const { return m_dist_sq; }
+    void insert(double dist_sq, kdnode* node)
+    {
+        if (!m_full || m_dist_sq > dist_sq)
+        {
+            m_dist_sq = dist_sq;
+            m_node = node;
+            m_full = true;
+        }
+    }
+    kdnode* node() const { return m_node; }
+private:
+    bool m_full;
+    double m_dist_sq;
+    kdnode* m_node;
+};
+
+
+void kd_nearest_i(kdnode *node, const AttributeType *pos,
+    SearchResults& result, DistanceType* sq_distances, DistanceType total_distance, int dir)
+{
+    kdnode *nearer_subtree, *farther_subtree;
+
+    /* Decide whether to go left or right in the tree */
+    const DistanceType dist = pos[dir] - node->pos[dir];
+    if (dist <= 0) {
+        nearer_subtree = node->left;
+        farther_subtree = node->right;
+    }
+    else {
+        nearer_subtree = node->right;
+        farther_subtree = node->left;
+    }
+
+    const auto new_dir = (dir + 1) % DIM;
+
+    if (nearer_subtree) {
+        /* Recurse down into nearer subtree */
+        kd_nearest_i(nearer_subtree, pos, result, sq_distances, total_distance, new_dir);
+    }
+
+    const auto sq_dist = SQ(dist);
+    total_distance += sq_dist - sq_distances[dir];
+    if (!result.isFull() || total_distance < result.dist_sq())
+    {
+        /* Check the distance of the point at the current node, compare it with our bests so far */
+        //double dist_sq = sq_dist + SQ(node->pos[1 - dir] - pos[1 - dir]);
+        DistanceType dist_sq = 0;
+        for (int i = 0; i < DIM; ++i)
+            dist_sq += SQ(node->pos[i] - pos[i]);
+
+        result.insert(dist_sq, node);
+
+        if (farther_subtree)
+        {
+            auto save_dist = sq_distances[dir];
+            sq_distances[dir] = sq_dist;
+
+            /* Recurse down into farther subtree */
+            kd_nearest_i(farther_subtree, pos, result, sq_distances, total_distance, new_dir);
+
+            sq_distances[dir] = save_dist;
+        }
+    }
+}
+
+
+void kd_nearest_i_nearer_subtree(kdnode *node, const AttributeType *pos,
+    SearchResults& result, bool* flags,
+    int dir)
+{
+    kdnode *nearer_subtree, *farther_subtree;
+    int flagIdx;
+
+    /* Decide whether to go left or right in the tree */
+    const DistanceType dist = pos[dir] - node->pos[dir];
+    if (dist <= 0) {
+        nearer_subtree = node->left;
+        farther_subtree = node->right;
+        flagIdx = dir * 2;
+    }
+    else {
+        nearer_subtree = node->right;
+        farther_subtree = node->left;
+        flagIdx = dir * 2 + 1;
+    }
+
+    const auto new_dir = (dir + 1) % DIM;
+
+    if (nearer_subtree) {
+        /* Recurse down into nearer subtree */
+        kd_nearest_i_nearer_subtree(nearer_subtree, pos, result, flags, new_dir);
+    }
+
+    if (flags[flagIdx])
+        return;
+
+    const auto sq_dist = SQ(dist);
+    if (!result.isFull() || sq_dist < result.dist_sq())
+    {
+        //if (node->pos != pos)
+        {
+            /* Check the distance of the point at the current node, compare it with our bests so far */
+            //double dist_sq = sq_dist + SQ(node->pos[1 - dir] - pos[1 - dir]);
+            DistanceType dist_sq = 0;
+            for (int i = 0; i < DIM; ++i)
+                dist_sq += SQ(node->pos[i] - pos[i]);
+
+            result.insert(dist_sq, node);
+        }
+
+        if (farther_subtree)
+        {
+            DistanceType sq_distances[DIM]{ };
+            //sq_distances[1 - dir] = 0;
+            sq_distances[dir] = sq_dist;
+
+            /* Recurse down into farther subtree */
+            kd_nearest_i(farther_subtree, pos, result, sq_distances, sq_dist, new_dir);
+        }
+    }
+    else
+        flags[flagIdx] = true;
+}
